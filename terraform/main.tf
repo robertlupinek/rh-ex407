0c679d7adfa5bf38d5c7846958f3508fc036b3e9
Robert Lupinek <darthlupi@gmail.com>
2020-03-16 20:28:39 -0400

    Adding set private IP addresses and allowing public IP addresses on non-jump hosts.  Too cheap for Nat instance. :)

--- terraform/main.tf
+++ terraform/main.tf
@@ -153,6 +153,8 @@ resource "aws_instance" "ansible_host_01" {
   instance_type = "t2.micro"
   vpc_security_group_ids = ["${aws_security_group.devops_hosts.id}"]
   subnet_id = aws_subnet.devops.id
+  associate_public_ip_address = true
+  private_ip = 10.0.0.80
   key_name = var.key_name
   root_block_device {
     delete_on_termination = true
@@ -169,6 +171,8 @@ resource "aws_instance" "ansible_host_02" {
   instance_type = "t2.micro"
   vpc_security_group_ids = ["${aws_security_group.devops_hosts.id}"]
   subnet_id = aws_subnet.devops.id
+  associate_public_ip_address = true
+  private_ip = 10.0.0.232
   key_name = var.key_name
   root_block_device {
     delete_on_termination = true
